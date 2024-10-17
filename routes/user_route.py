728da56b0c3ee436ea82c4d1b8756640a42cf933
Mathues <obrmatheusrantin@gmail.com>
2024-10-17 16:46:35 -0300

    Primeito Commit

--- routes/user_route.py
+++ routes/user_route.py
@@ -1,18 +1,31 @@
 from flask import Flask, render_template, redirect, url_for, request, session, Blueprint
-from serverDatabase import getUser
 from models import Book
+from serverDatabase import getInfoItems, getLocais, getIdItems, getIdLocais, registerItem_locais
 
 user_bp = Blueprint('user', __name__ , template_folder='templates')
 
 @user_bp.route('/')
 def home():
+    lista_locais = getLocais()
     name = session.get('user')
-    return render_template('home.html', name= name)
 
-@user_bp.route('/register-book')
-def register_book():
-    return render_template('register_book.html')
+    return render_template('home.html', name= name, lista_locais = lista_locais)
 
+@user_bp.route('/cad', methods=["POST","GET"])
+def cadastrarItems():
+    locais = getIdLocais()
+    items = getIdItems()
+    dados = getLocais()
+    return render_template('registerItem.html', locais = locais, items = items, dados = dados)
+
+@user_bp.route('/add_item_local', methods=['GET', 'POST'])
+def addItemLocal():
+    local = request.form.get('local_id')
+    item = request.form.get('item_id')
+    qnt = request.form.get('quantidade')
+
+    requestDb = registerItem_locais(local, item, qnt)
+    return redirect('/', vf = requestDb)
 
 
 @user_bp.before_request
--- serverDatabase.py
+++ serverDatabase.py
@@ -1,7 +1,7 @@
 import psycopg2
 
 conn = psycopg2.connect(
-                        database='LivrariaMarialva',
+                        database='PatrimonioPrefeitura',
                         host="localhost",
                         port=5432,
                         user='postgres',
@@ -15,7 +15,7 @@ def getUser(user):
         print('Erro, valores invalidos ')
 
     cur = conn.cursor()
-    SQL = f"SELECT email FROM users WHERE email ='{email}' AND senha = '{pwd}'"
+    SQL = f"SELECT email FROM usuarios WHERE email ='{email}' AND senha = '{pwd}'"
     cur.execute(SQL)
     response_database = cur.fetchall()
     try:
@@ -26,7 +26,7 @@ def getUser(user):
 
 def getNameUser(email):
     cur= conn.cursor()
-    SQL = f"SELECT nome FROM users WHERE email = '{email}';"
+    SQL = f"SELECT nome FROM usuarios WHERE email = '{email}';"
     cur.execute(SQL)
     response_database = cur.fetchall()
     try:
@@ -35,3 +35,50 @@ def getNameUser(email):
         return response_database
     except IndexError:
         return "Erro"
+
+def getInfoItems(local, modelo):
+    cur = conn.cursor() 
+    SQL= f"SELECT SUM(quantidade) FROM items WHERE modelo = '{modelo}' AND localizacao = '{local}';"
+    cur.execute(SQL)
+    response_db = cur.fetchall()
+    return response_db
+
+def getLocais():
+    cur = conn.cursor()
+    SQL ='''
+       SELECT l.id AS local_id,  -- Pega o ID do local
+       l.nome AS local_nome,
+       COALESCE(SUM(CASE WHEN i.nome = 'Impressoras' THEN il.quantidade END), 0) AS quantidade_impressora,
+       COALESCE(SUM(CASE WHEN i.nome = 'Computadores' THEN il.quantidade END), 0) AS quantidade_computador,
+       COALESCE(SUM(CASE WHEN i.nome = 'Monitores' THEN il.quantidade END), 0) AS quantidade_monitor
+        FROM locais l
+        LEFT JOIN itens_locais il ON l.id = il.local_id
+        LEFT JOIN itens i ON il.item_id = i.id
+        GROUP BY l.id, l.nome;  -- Agrupar também pelo ID do local
+
+
+        '''
+    cur.execute(SQL)
+    dados = cur.fetchall()
+    return dados
+    
+def getIdLocais():
+    cur = conn.cursor()
+    SQL = "SELECT id, nome FROM locais"
+    cur.execute(SQL)
+    response = cur.fetchall()
+    return response
+
+def getIdItems():
+    cur = conn.cursor()
+    SQL = "SELECT id, nome FROM itens"
+    cur.execute(SQL)
+    response = cur.fetchall()
+    return response
+
+def registerItem_locais(id_local, id_item, quantidade):
+    cur = conn.cursor()
+    SQL = f"INSERT INTO itens_locais VALUES(DEFAULT, '{id_item}', '{id_local}', {quantidade}) "
+    cur.execute(SQL)
+    conn.commit()
+    return True
